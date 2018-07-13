2d308d5f83c16f28ba57027cc1af94390b2185cf
nbesteman <nbesteman@gmail.com>
2018-07-13 16:55:08 -0400

    Edit GeoAdd2

---
+++ addresses/processing/EditTableGeoAddress2.py
@@ -0,0 +1,26 @@
+#-------------------------------------------------------------------------------
+# Name:     edit_table_GeoAddress2.py
+# Purpose:  Edit table schema.
+# Author:   Neil Besteman
+# Created:  20180713
+# Modified:
+#-------------------------------------------------------------------------------
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
+import arcpy
+#delete primary address fields
+
+
+arcpy.DeleteField_management(in_table="GeoAdd2", drop_field="NUMBER;PREDIR;NAME;SUFFIX;POSTDIR;SUPPLEM")
+
+#rename fields
+arcpy.AlterField_management(in_table="J:\Apps\Python\LayerUpdates\addresses\source\AddressData.gdb\GeoAdd2", field="NUMBER1", new_field_name="NUMBER", new_field_alias="", field_type="TEXT", field_length="5", field_is_nullable="NULLABLE", clear_field_alias="true")
+arcpy.AlterField_management(in_table="J:\Apps\Python\LayerUpdates\addresses\source\AddressData.gdb\GeoAdd2", field="NAME1", new_field_name="NAME", new_field_alias="", field_type="TEXT", field_length="40", field_is_nullable="NULLABLE", clear_field_alias="true")
+arcpy.AlterField_management(in_table="J:\Apps\Python\LayerUpdates\addresses\source\AddressData.gdb\GeoAdd2", field="PREDIR1", new_field_name="PREDIR", new_field_alias="", field_type="TEXT", field_length="2", field_is_nullable="NULLABLE", clear_field_alias="true")
+arcpy.AlterField_management(in_table="J:\Apps\Python\LayerUpdates\addresses\source\AddressData.gdb\GeoAdd2", field="SUFFIX1", new_field_name="SUFFIX", new_field_alias="", field_type="TEXT", field_length="4", field_is_nullable="NULLABLE", clear_field_alias="true")
+arcpy.AlterField_management(in_table="J:\Apps\Python\LayerUpdates\addresses\source\AddressData.gdb\GeoAdd2", field="POSTIR1", new_field_name="POSTDIR", new_field_alias="", field_type="TEXT", field_length="2", field_is_nullable="NULLABLE", clear_field_alias="true")
+arcpy.AlterField_management(in_table="J:\Apps\Python\LayerUpdates\addresses\source\AddressData.gdb\GeoAdd2", field="SUPPLEM1", new_field_name="SUPPLEM", new_field_alias="", field_type="TEXT", field_length="30", field_is_nullable="NULLABLE", clear_field_alias="true")
